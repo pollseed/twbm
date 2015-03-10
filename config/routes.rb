dcc1ae04244c3e99ee0647d856b4513734ac83a8
pollseed <cb.so.devope+0001@gmail.com>
2015-03-10 09:33:33 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,15 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  namespace "admin" do
+    # ADMIN　TOP
+    get 'manage' => 'manage#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    # BOT
+    get 'manage/bot' => 'manage#bot'
+    post 'manage/bot' => 'manage#bot'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    # TWITTER
+    get 'manage/bot/twitter' => 'manage#twitter'
+    post 'manage/bot/twitter' => 'manage#twitter'
+    delete 'manage/bot/twitter' => 'manage#twitter'
+  end
 end
