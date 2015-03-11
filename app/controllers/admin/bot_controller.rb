1bcc9b4bb8d12cee886d3dc256ff75d8946a945b
pollseed <cb.so.devope+0001@gmail.com>
2015-03-12 05:33:10 +0900

    add replicate && a littlittle refactor

--- app/controllers/admin/bot_controller.rb
+++ app/controllers/admin/bot_controller.rb
@@ -1,4 +1,7 @@
 class Admin::BotController < ApplicationController
+
+  NG = "ng"
+
   def index
     list
     @bot = Bot.new
@@ -8,49 +11,42 @@ class Admin::BotController < ApplicationController
     list
     @bot = Bot.new(bot_params)
     if @bot.nil?
-      render({action: :index}, alert: "登録失敗!!")
+      render('admin/bot/index', alert: "登録失敗!!")
     end
 
     if @bot.save
-      redirect_to({action: :index}, notice: "登録完了!!")
+      redirect_to(admin_bot_index_path, notice: "登録完了!!")
     else
-      render '/admin/manage/bot'
+      render 'admin/bot/index'
     end
   end
 
-  def list
-    @bots = Bot.find_by
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def show
-  end
-
-  def update
-  end
-
   def destroy
-    bot = Bot.find_by_id(params[:id])
-    bot.twitter_id = bot.twitter_id.blank? ? "ng" : bot.twitter_id
-    bot.twitter_name = bot.twitter_name.blank? ? "ng" : bot.twitter_name
-    bot.access_token = bot.access_token.blank? ? "ng" : bot.access_token
-    bot.hash_tags = bot.hash_tags.blank? ? "ng" : bot.hash_tags
-    bot.deleted = true
+    bot = find_destroy_bot
     if bot.save
       flash[:delete] =  "物理削除完了!!"
-      redirect_to({action: :index})
+      redirect_to admin_bot_index_path
     else
-      render '/admin/manage/bot'
+      render 'admin/bot/index'
     end
   end
 
   private
     def bot_params
       params.require(:bot).permit(:twitter_name, :twitter_id, :access_token, :hash_tags)
+    end
+
+    def list
+      @bots = Bot.find_by
+    end
+
+    def find_destroy_bot
+      bot = Bot.find_by_id(params[:id])
+      bot.twitter_id = bot.twitter_id.blank? ? NG : bot.twitter_id
+      bot.twitter_name = bot.twitter_name.blank? ? NG : bot.twitter_name
+      bot.access_token = bot.access_token.blank? ? NG : bot.access_token
+      bot.hash_tags = bot.hash_tags.blank? ? NG : bot.hash_tags
+      bot.deleted = true
+      bot
     end
 end
--- app/controllers/admin/manage_controller.rb
+++ app/controllers/admin/manage_controller.rb
@@ -1,16 +1,4 @@
 class Admin::ManageController < ApplicationController
   def index
   end
-
-  def delete
-
-  end
-
-  def twitter
-  end
-
-  private
-    def bot_params
-      params.require(:bot).permit(:twitter_name, :twitter_id, :access_token, :hash_tags)
-    end
 end
