a1c93f370deb77322f8eea4456ef54f120c54855
Gaddict <ksano6253@gmail.com>
2015-03-29 18:46:04 +0900

    fix spec require

--- spec/controllers/admin/bot_hash_tag_rels_controller_spec.rb
+++ spec/controllers/admin/bot_hash_tag_rels_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Admin::BotHashTagRelsController, type: :controller do
 
--- spec/helpers/admin/bot_hash_tag_rels_helper_spec.rb
+++ spec/helpers/admin/bot_hash_tag_rels_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
 # the Admin::BotHashTagRelsHelper. For example:
@@ -11,5 +11,5 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe Admin::BotHashTagRelsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
