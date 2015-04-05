29729a60ca1fb8789e04b21598d2c89aeb2e5af9
Gaddict <ksano6253@gmail.com>
2015-04-06 01:06:39 +0900

    issue close #7

--- app/helpers/admin/bot_helper.rb
+++ app/helpers/admin/bot_helper.rb
@@ -6,4 +6,12 @@ module Admin::BotHelper
   def bots
     @bots ||= Bot.find_by
   end
+
+  def get_twitter_name
+    session[:twitter_name].nil? ? nil : session[:twitter_name]
+  end
+
+  def get_twitter_id
+    session[:twitter_id].nil? ? nil : "@"+session[:twitter_id]
+  end
 end
